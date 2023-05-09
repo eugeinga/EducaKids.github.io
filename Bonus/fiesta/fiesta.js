e2d41349c6fad28995967e6aa17af18f1c6bb857
orlandoc0107 <orlandocardenas0107@gmail.com>
2023-05-09 19:27:46 -0300

    cambios de url

--- Bonus/fiesta/fiesta.js
+++ Bonus/fiesta/fiesta.js
@@ -1,47 +1,4 @@
-
-// llamadas de Estructuras
-
-
-/* Header de la Pagina  */
-
-fetch("/body/header.html")
-.then(response => response.text())
-.then(data => {
-  const headerContainer = document.getElementById('header-container');
-  headerContainer.innerHTML = data;
-});
-
-/* Nav de la Pagina  */
-
-fetch("/body/nav.html")
-.then(response => response.text())
-.then(data => {
-  const navContainer = document.getElementById('nav-container');
-  navContainer.innerHTML = data;
-});
-
-/* Section de la Pagina  */
-
-
-/* Aside de la Pagina  */
-
-fetch("/body/aside.html")
-.then(response => response.text())
-.then(data => {
-  const asideContainer = document.getElementById('aside-container');
-  asideContainer.innerHTML = data;
-});
-
-/* Footer de la Pagina  */
-
-fetch("/body/footer.html")
-.then(response => response.text())
-.then(data => {
-  const footerContainer = document.getElementById('footer-container');
-  footerContainer.innerHTML = data;
-});
-
-//  Calculos Para los Presuepuestos 
+/* */
 
 function calcularTotal() {
   let total = 0;
