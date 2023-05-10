81fa65b09008c6b28845c4590854dce4e0ec9481
orlandoc0107 <orlandocardenas0107@gmail.com>
2023-05-10 19:45:29 -0300

    modificaciones del nav

--- Acerca/index.js
+++ Acerca/index.js
@@ -3,6 +3,6 @@
 function mostrarMenu() {
     let botonMenu = document.querySelector('#boton-menu');
     let nav = document.querySelector('nav');
-    nav.style.left = nav.style.left === '-250px' ? '0' : '-250px';
+    nav.style.left = nav.style.left === '-450px' ? '0' : '-450px';
     localStorage.setItem('menuOpen', nav.style.left === '0' ? 'true' : 'false');
 }
