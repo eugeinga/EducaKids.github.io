2bc34ceb54b01fee743f76431ce41c98f5266d97
orlandoc0107 <orlandocardenas0107@gmail.com>
2023-05-18 04:09:47 -0300

    vue map google.

--- Aprender/geografia/index.js
+++ Aprender/geografia/index.js
@@ -8,20 +8,23 @@ function mostrarMenu() {
 }
 
 
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
-  
-  window.initMap = initMap;
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            map: null,
+        };
+    },
+    mounted() {
+        this.initMap();
+    },
+    methods: {
+        initMap() {
+            this.map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 37.7749, lng: -122.4194 }, // Coordenadas para centrar el mapa
+                zoom: 12, // Nivel de zoom inicial
+            });
+        },
+    },
+});
