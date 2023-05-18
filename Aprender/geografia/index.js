4dbb99ad0f428100ff79ff0aadf1286624d5f055
orlandoc0107 <orlandocardenas0107@gmail.com>
2023-05-18 02:17:03 -0300

    cambios Geografia API

--- Aprender/geografia/index.js
+++ Aprender/geografia/index.js
@@ -6,3 +6,14 @@ function mostrarMenu() {
     nav.style.left = nav.style.left === '-450px' ? '0' : '-450px';
     localStorage.setItem('menuOpen', nav.style.left === '0' ? 'true' : 'false');
 }
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
