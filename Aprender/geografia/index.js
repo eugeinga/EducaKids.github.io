d6aba701f135a4f2958f9ce8f5de06dc51268155
orlandoc0107 <orlandocardenas0107@gmail.com>
2023-05-18 03:36:04 -0300

    url cambios

--- Aprender/geografia/index.js
+++ Aprender/geografia/index.js
@@ -11,10 +11,10 @@ function mostrarMenu() {
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
 }
 
 window.initMap = initMap;
