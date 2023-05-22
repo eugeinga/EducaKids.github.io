9b79ba8763489cdc324798118826a6f61b44209c
orlandoc0107 <orlandocardenas0107@gmail.com>
2023-05-22 15:53:47 -0300

    Matematica y Lengua

--- Aprender/lengua/index.js
+++ Aprender/lengua/index.js
@@ -1,99 +1,175 @@
 //Funcion para Mostrar o ocultar el Nav
 
-function mostrarMenu() {
-    let botonMenu = document.querySelector('#boton-menu');
-    let nav = document.querySelector('nav');
-    nav.style.left = nav.style.left === '-450px' ? '0' : '-450px';
-    localStorage.setItem('menuOpen', nav.style.left === '0' ? 'true' : 'false');
-}
+//function mostrarMenu() {
+//  let botonMenu = document.querySelector('#boton-menu');
+//  let nav = document.querySelector('nav');
+//  nav.style.left = nav.style.left === '-450px' ? '0' : '-450px';
+//  localStorage.setItem('menuOpen', nav.style.left === '0' ? 'true' : 'false');
+//}
+
+
 
 ///Creacion de Variables
 
-var videos = document.querySelector('#video');
+var video = document.querySelector('#video');
+var detalle = document.querySelector('#detalle');
 
-/////////////// Videos
-
-
-function video1() {
-  fetch('videos/video1.txt')
-    .then(data => data.text())
-    .then(data => {
-      videos.innerHTML = `${data}`
-    })
+//// funciones
+function uno() {
+    fetch('videos/uno.txt')
+        .then(data => data.text())
+        .then(data => {
+            video.innerHTML = `${data}`
+        })
+    
+        fetch('videos/unoD.txt')
+        .then(data => data.text())
+        .then(data => {
+            detalle.innerHTML = `${data}`
+        })
 }
 
-function video2() {
-    fetch('videos/video2.txt')
+///
+function dos() {
+  fetch('videos/dos.txt')
       .then(data => data.text())
       .then(data => {
-        videos.innerHTML = `${data}`
+          video.innerHTML = `${data}`
       })
-  }
-
-  function video3() {
-    fetch('videos/video3.txt')
+  
+      fetch('videos/dosD.txt')
       .then(data => data.text())
       .then(data => {
-        videos.innerHTML = `${data}`
+          detalle.innerHTML = `${data}`
       })
-  }
+}
 
-  function video4() {
-    fetch('videos/video4.txt')
+///
+
+function tres() {
+  fetch('videos/tres.txt')
       .then(data => data.text())
       .then(data => {
-        videos.innerHTML = `${data}`
+          video.innerHTML = `${data}`
       })
-  }
-
-  function video5() {
-    fetch('videos/video5.txt')
+  
+      fetch('videos/tresD.txt')
       .then(data => data.text())
       .then(data => {
-        videos.innerHTML = `${data}`
+          detalle.innerHTML = `${data}`
       })
-  }
+}
 
-  function video6() {
-    fetch('videos/video6.txt')
+///
+
+function cuatro() {
+  fetch('videos/cuatro.txt')
       .then(data => data.text())
       .then(data => {
-        videos.innerHTML = `${data}`
+          video.innerHTML = `${data}`
       })
-  }
-
-  function video7() {
-    fetch('videos/video7.txt')
+  
+      fetch('videos/cuatroD.txt')
       .then(data => data.text())
       .then(data => {
-        videos.innerHTML = `${data}`
+          detalle.innerHTML = `${data}`
       })
-  }
+}
 
-  function video8() {
-    fetch('videos/video8.txt')
+///
+
+function cinco() {
+  fetch('videos/cinco.txt')
       .then(data => data.text())
       .then(data => {
-        videos.innerHTML = `${data}`
+          video.innerHTML = `${data}`
       })
-  }
-
-  function video9() {
-    fetch('videos/video9.txt')
+  
+      fetch('videos/cincoD.txt')
       .then(data => data.text())
       .then(data => {
-        videos.innerHTML = `${data}`
+          detalle.innerHTML = `${data}`
       })
-  }
+}
 
-  function video10() {
-    fetch('videos/video10.txt')
+///
+
+function seis() {
+  fetch('videos/seistxt')
       .then(data => data.text())
       .then(data => {
-        videos.innerHTML = `${data}`
+          video.innerHTML = `${data}`
       })
-  }
+  
+      fetch('videos/seisD.txt')
+      .then(data => data.text())
+      .then(data => {
+          detalle.innerHTML = `${data}`
+      })
+}
 
-  ///// ////////
+///
 
+function siete() {
+  fetch('videos/siete.txt')
+      .then(data => data.text())
+      .then(data => {
+          video.innerHTML = `${data}`
+      })
+  
+      fetch('videos/sieteD.txt')
+      .then(data => data.text())
+      .then(data => {
+          detalle.innerHTML = `${data}`
+      })
+}
 
+///
+
+function ocho() {
+  fetch('videos/ocho.txt')
+      .then(data => data.text())
+      .then(data => {
+          video.innerHTML = `${data}`
+      })
+  
+      fetch('videos/ochoD.txt')
+      .then(data => data.text())
+      .then(data => {
+          detalle.innerHTML = `${data}`
+      })
+}
+
+///
+
+function nueve() {
+  fetch('videos/nueve.txt')
+      .then(data => data.text())
+      .then(data => {
+          video.innerHTML = `${data}`
+      })
+  
+      fetch('videos/nueveD.txt')
+      .then(data => data.text())
+      .then(data => {
+          detalle.innerHTML = `${data}`
+      })
+}
+
+///
+
+function diez() {
+  fetch('videos/diez.txt')
+      .then(data => data.text())
+      .then(data => {
+          video.innerHTML = `${data}`
+      })
+  
+      fetch('videos/diezD.txt')
+      .then(data => data.text())
+      .then(data => {
+          detalle.innerHTML = `${data}`
+      })
+}
+
+///
